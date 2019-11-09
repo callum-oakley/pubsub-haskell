e3aa189f0507523143b54213111935c1895456ce
Callum Oakley <c.oakley108@gmail.com>
2019-11-09 22:04:15 +0000

    use Handle interface so we can read a line at a time

--- src/Request.hs
+++ src/Request.hs
@@ -5,11 +5,10 @@ module Request
   , newRequest
   ) where
 
-import           Data.Text                 (Text)
-import qualified Data.Text                 as Text
-import qualified Data.Text.Encoding        as Encoding
-import           Network.Socket            (Socket)
-import qualified Network.Socket.ByteString as SBS
+import           Data.Text    (Text)
+import qualified Data.Text    as Text
+import qualified Data.Text.IO as TIO
+import           System.IO    (Handle)
 
 import qualified Bus
 
@@ -18,19 +17,15 @@ data Request
   | Sub Bus.Channel
   deriving (Show)
 
-newRequest :: Socket -> IO (Either Text Request)
-newRequest socket = do
-  op <- readText socket
+newRequest :: Handle -> IO (Either Text Request)
+newRequest conn = do
+  op <- Text.strip <$> TIO.hGetLine conn
   case op of
     "PUB" -> do
-      channel <- readText socket
-      message <- readText socket
+      channel <- Text.strip <$> TIO.hGetLine conn
+      message <- Text.strip <$> TIO.hGetLine conn
       return $ Right $ Pub channel message
     "SUB" -> do
-      channel <- readText socket
+      channel <- Text.strip <$> TIO.hGetLine conn
       return $ Right $ Sub channel
-    otherwise -> return $ Left $ Text.append "Unknown operation: " op
-
--- TODO handle messages larger than 4096 bytes
-readText :: Socket -> IO Text
-readText socket = Text.strip <$> Encoding.decodeUtf8 <$> SBS.recv socket 4096
+    _ -> return $ Left $ Text.append "Unknown operation: " op
--- src/Server.hs
+++ src/Server.hs
@@ -5,21 +5,24 @@ module Server
   , listen
   ) where
 
-import           Control.Concurrent.Async  (async)
-import qualified Control.Concurrent.STM    as STM
-import           Control.Monad             (forever)
-import qualified Data.Text                 as Text
-import qualified Data.Text.Encoding        as Encoding
-import           Network.Socket            (Socket)
-import qualified Network.Socket            as Socket
-import qualified Network.Socket.ByteString as SBS
+import           Control.Concurrent.Async (async)
+import qualified Control.Concurrent.STM   as STM
+import           Control.Monad            (forever)
+import qualified Data.List                as List
+import           Data.Text                (Text)
+import qualified Data.Text                as Text
+import qualified Data.Text.IO             as TIO
+import           Network.Socket           (Socket)
+import qualified Network.Socket           as Socket
+import           System.IO                (Handle)
+import qualified System.IO                as IO
 
-import           Bus                       (Bus)
+import           Bus                      (Bus)
 import qualified Bus
-import           Request                   (Request (..))
+import           Request                  (Request (..))
 import qualified Request
 
-data Server =
+newtype Server =
   Server Bus
 
 bus :: Server -> Bus
@@ -27,34 +30,40 @@ bus (Server b) = b
 
 listen :: Server -> Socket -> IO ()
 listen server socket = do
-  (conn, _) <- Socket.accept socket
+  (socket', _) <- Socket.accept socket
+  conn <- Socket.socketToHandle socket' IO.ReadWriteMode
   _ <- async $ handle server conn
   listen server socket
 
-handle :: Server -> Socket -> IO ()
+handle :: Server -> Handle -> IO ()
 handle server conn = do
   request <- Request.newRequest conn
   case request of
     Left err -> do
-      SBS.sendAll conn $
-        Encoding.encodeUtf8 $
+      logTexts ["ERR", err]
+      TIO.hPutStr conn $
         Text.concat ["ERR\r\nfailed to parse request: ", err, "\r\n"]
       handle server conn
     Right (Pub channel message) -> handlePub server conn channel message
     Right (Sub channel) -> handleSub server conn channel
 
-handlePub :: Server -> Socket -> Bus.Channel -> Bus.Message -> IO ()
+handlePub :: Server -> Handle -> Bus.Channel -> Bus.Message -> IO ()
 handlePub server conn channel message = do
-  SBS.sendAll conn "ACK\r\n"
+  logTexts ["PUB", channel, message]
+  TIO.hPutStr conn "ACK\r\n"
   STM.atomically $ Bus.publish (bus server) channel message
   handle server conn
 
 -- TODO handle disconnects
-handleSub :: Server -> Socket -> Bus.Channel -> IO ()
+handleSub :: Server -> Handle -> Bus.Channel -> IO ()
 handleSub server conn channel = do
-  SBS.sendAll conn "ACK\r\n"
+  logTexts ["SUB", channel]
+  TIO.hPutStr conn "ACK\r\n"
   queue <- STM.atomically $ Bus.subscribe (bus server) channel
   forever $ do
     message <- STM.atomically $ STM.readTQueue queue
-    SBS.sendAll conn $
-      Encoding.encodeUtf8 $ Text.concat ["MSG\r\n", message, "\r\n"]
+    TIO.hPutStr conn $ Text.concat ["MSG\r\n", message, "\r\n"]
+
+logTexts :: [Text] -> IO ()
+logTexts ts =
+  TIO.putStr $ Text.append (Text.concat $ List.intersperse " " ts) "\n"
